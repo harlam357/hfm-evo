0ee3e71516c4b772833480e657ad35f8aa70408c
harlam357 <harlam357@gmail.com>
2022-12-07 19:49:37 -0600

    refactor: logger extensions to more closely match MS LoggerExtensions

--- src/HFM.Core/Logging/LoggerExtensions.cs
+++ src/HFM.Core/Logging/LoggerExtensions.cs
@@ -1,16 +1,31 @@
-﻿namespace HFM.Core.Logging;
+﻿using System.Globalization;
 
+namespace HFM.Core.Logging;
+
+[System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
 public static class LoggerExtensions
 {
-    public static void Debug(this ILogger logger, string message, Exception? exception = null) =>
-        logger.Log(LoggerLevel.Debug, message, exception);
+    public static void Debug(this ILogger logger, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Debug, String.Format(CultureInfo.CurrentCulture, message, args));
 
-    public static void Info(this ILogger logger, string message, Exception? exception = null) =>
-        logger.Log(LoggerLevel.Info, message, exception);
+    public static void Debug(this ILogger logger, Exception? exception, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Debug, String.Format(CultureInfo.CurrentCulture, message, args), exception);
 
-    public static void Warn(this ILogger logger, string message, Exception? exception = null) =>
-        logger.Log(LoggerLevel.Warn, message, exception);
+    public static void Info(this ILogger logger, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Info, String.Format(CultureInfo.CurrentCulture, message, args));
 
-    public static void Error(this ILogger logger, string message, Exception? exception = null) =>
-        logger.Log(LoggerLevel.Error, message, exception);
+    public static void Info(this ILogger logger, Exception? exception, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Info, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+
+    public static void Warn(this ILogger logger, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Warn, String.Format(CultureInfo.CurrentCulture, message, args));
+
+    public static void Warn(this ILogger logger, Exception? exception, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Warn, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+
+    public static void Error(this ILogger logger, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Error, String.Format(CultureInfo.CurrentCulture, message, args));
+
+    public static void Error(this ILogger logger, Exception? exception, string message, params object?[] args) =>
+        logger.Log(LoggerLevel.Error, String.Format(CultureInfo.CurrentCulture, message, args), exception);
 }
