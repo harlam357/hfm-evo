931a219ff0f674354931abe339bd5df10095e277
harlam357 <harlam357@gmail.com>
2022-12-23 09:43:48 -0600

    init: ProjectInfo and extensions

---
+++ src/HFM.Core.Tests/WorkUnits/ProjectInfoTests.cs
@@ -0,0 +1,158 @@
+﻿namespace HFM.Core.WorkUnits;
+
+[TestFixture]
+public class ProjectInfoTests
+{
+    [TestCase(0, 0, 0, 0, ExpectedResult = false)]
+    [TestCase(1, 0, 0, 0, ExpectedResult = true)]
+    [TestCase(0, 1, 0, 0, ExpectedResult = true)]
+    [TestCase(0, 0, 1, 0, ExpectedResult = true)]
+    [TestCase(0, 0, 0, 1, ExpectedResult = true)]
+    public bool HasProject(int id, int run, int clone, int gen) =>
+        new ProjectInfo
+        {
+            ProjectId = id,
+            ProjectRun = run,
+            ProjectClone = clone,
+            ProjectGen = gen
+        }.HasProject();
+
+    [TestCase(0, 0, 0, 0, ExpectedResult = "Project: 0 (Run 0, Clone 0, Gen 0)")]
+    [TestCase(1, 0, 0, 0, ExpectedResult = "Project: 1 (Run 0, Clone 0, Gen 0)")]
+    [TestCase(0, 1, 0, 0, ExpectedResult = "Project: 0 (Run 1, Clone 0, Gen 0)")]
+    [TestCase(0, 0, 1, 0, ExpectedResult = "Project: 0 (Run 0, Clone 1, Gen 0)")]
+    [TestCase(0, 0, 0, 1, ExpectedResult = "Project: 0 (Run 0, Clone 0, Gen 1)")]
+    public string ToString(int id, int run, int clone, int gen) =>
+        new ProjectInfo
+        {
+            ProjectId = id,
+            ProjectRun = run,
+            ProjectClone = clone,
+            ProjectGen = gen
+        }.ToString();
+
+    [TestCase(0, 0, 0, 0, ExpectedResult = "Project: 0 (Run 0, Clone 0, Gen 0)")]
+    [TestCase(1, 0, 0, 0, ExpectedResult = "Project: 1 (Run 0, Clone 0, Gen 0)")]
+    [TestCase(0, 1, 0, 0, ExpectedResult = "Project: 0 (Run 1, Clone 0, Gen 0)")]
+    [TestCase(0, 0, 1, 0, ExpectedResult = "Project: 0 (Run 0, Clone 1, Gen 0)")]
+    [TestCase(0, 0, 0, 1, ExpectedResult = "Project: 0 (Run 0, Clone 0, Gen 1)")]
+    public string ToProjectString(int id, int run, int clone, int gen) =>
+        new ProjectInfo
+        {
+            ProjectId = id,
+            ProjectRun = run,
+            ProjectClone = clone,
+            ProjectGen = gen
+        }.ToProjectString();
+
+    [TestCase(0, 0, 0, 0, ExpectedResult = "P0 (R0, C0, G0)")]
+    [TestCase(1, 0, 0, 0, ExpectedResult = "P1 (R0, C0, G0)")]
+    [TestCase(0, 1, 0, 0, ExpectedResult = "P0 (R1, C0, G0)")]
+    [TestCase(0, 0, 1, 0, ExpectedResult = "P0 (R0, C1, G0)")]
+    [TestCase(0, 0, 0, 1, ExpectedResult = "P0 (R0, C0, G1)")]
+    public string ToShortProjectString(int id, int run, int clone, int gen) =>
+        new ProjectInfo
+        {
+            ProjectId = id,
+            ProjectRun = run,
+            ProjectClone = clone,
+            ProjectGen = gen
+        }.ToShortProjectString();
+
+    [TestFixture]
+    public class GivenProjectInfoIsNull
+    {
+        [Test]
+        public void ThenHasProjectReturnsFalse()
+        {
+            IProjectInfo? projectInfo = null;
+            Assert.That(projectInfo.HasProject(), Is.False);
+        }
+
+        [Test]
+        public void ThenToProjectStringReturnsEmptyString()
+        {
+            IProjectInfo? projectInfo = null;
+            Assert.That(projectInfo.ToProjectString(), Is.Empty);
+        }
+
+        [Test]
+        public void ThenToShortProjectStringReturnsEmptyString()
+        {
+            IProjectInfo? projectInfo = null;
+            Assert.That(projectInfo.ToShortProjectString(), Is.Empty);
+        }
+    }
+
+    [TestFixture]
+    public class GivenTwoNullProjectInfos
+    {
+        [Test]
+        public void ThenProjectsAreNotEqual()
+        {
+            IProjectInfo? projectInfo1 = null;
+            IProjectInfo? projectInfo2 = null;
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.False);
+        }
+    }
+
+    [TestFixture]
+    public class GivenFirstProjectInfoIsNull
+    {
+        [Test]
+        public void ThenProjectsAreNotEqual()
+        {
+            IProjectInfo? projectInfo1 = null;
+            var projectInfo2 = new ProjectInfo { ProjectId = 5, ProjectRun = 6, ProjectClone = 7, ProjectGen = 8 };
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.False);
+        }
+    }
+
+    [TestFixture]
+    public class GivenSecondProjectInfoIsNull
+    {
+        [Test]
+        public void ThenProjectsAreNotEqual()
+        {
+            var projectInfo1 = new ProjectInfo { ProjectId = 1, ProjectRun = 2, ProjectClone = 3, ProjectGen = 4 };
+            IProjectInfo? projectInfo2 = null;
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.False);
+        }
+    }
+
+    [TestFixture]
+    public class GivenTwoDefaultProjectInfos
+    {
+        [Test]
+        public void ThenProjectsAreEqual()
+        {
+            var projectInfo1 = new ProjectInfo();
+            var projectInfo2 = new ProjectInfo();
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.True);
+        }
+    }
+
+    [TestFixture]
+    public class GivenTwoProjectInfosWithSameProjectRunCloneGen
+    {
+        [Test]
+        public void ThenProjectsAreEqual()
+        {
+            var projectInfo1 = new ProjectInfo { ProjectId = 1, ProjectRun = 2, ProjectClone = 3, ProjectGen = 4 };
+            var projectInfo2 = new ProjectInfo { ProjectId = 1, ProjectRun = 2, ProjectClone = 3, ProjectGen = 4 };
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.True);
+        }
+    }
+
+    [TestFixture]
+    public class GivenTwoProjectInfosWithDifferentProjectRunCloneGen
+    {
+        [Test]
+        public void ThenProjectsAreNotEqual()
+        {
+            var projectInfo1 = new ProjectInfo { ProjectId = 1, ProjectRun = 2, ProjectClone = 3, ProjectGen = 4 };
+            var projectInfo2 = new ProjectInfo { ProjectId = 5, ProjectRun = 6, ProjectClone = 7, ProjectGen = 8 };
+            Assert.That(projectInfo1.EqualsProject(projectInfo2), Is.False);
+        }
+    }
+}
---
+++ src/HFM.Core/WorkUnits/ProjectInfo.cs
@@ -0,0 +1,66 @@
+﻿using System.Globalization;
+
+namespace HFM.Core.WorkUnits;
+
+public interface IProjectInfo
+{
+    int ProjectId { get; }
+    int ProjectRun { get; }
+    int ProjectClone { get; }
+    int ProjectGen { get; }
+}
+
+public sealed class ProjectInfo : IProjectInfo
+{
+    public int ProjectId { get; set; }
+    public int ProjectRun { get; set; }
+    public int ProjectClone { get; set; }
+    public int ProjectGen { get; set; }
+
+    public override string ToString() => this.ToProjectString();
+}
+
+public static class ProjectInfoExtensions
+{
+    /// <summary>
+    /// Is the project information populated?
+    /// </summary>
+    /// <returns>true if Project (R/C/G) has been identified; otherwise, false.</returns>
+    public static bool HasProject(this IProjectInfo? projectInfo) =>
+        projectInfo != null &&
+        (projectInfo.ProjectId != 0 ||
+         projectInfo.ProjectRun != 0 ||
+         projectInfo.ProjectClone != 0 ||
+         projectInfo.ProjectGen != 0);
+
+    /// <summary>
+    /// Determines whether the specified project information is equal to this project information.
+    /// </summary>
+    /// <returns>true if the specified Project (R/C/G) is equal to the this Project (R/C/G); otherwise, false.</returns>
+    internal static bool EqualsProject(this IProjectInfo? projectInfo1, IProjectInfo? projectInfo2) =>
+        projectInfo1 != null && projectInfo2 != null &&
+        projectInfo1.ProjectId == projectInfo2.ProjectId &&
+        projectInfo1.ProjectRun == projectInfo2.ProjectRun &&
+        projectInfo1.ProjectClone == projectInfo2.ProjectClone &&
+        projectInfo1.ProjectGen == projectInfo2.ProjectGen;
+
+    /// <summary>
+    /// Returns a string that represents the Project (R/C/G) information.
+    /// </summary>
+    public static string ToProjectString(this IProjectInfo? projectInfo)
+    {
+        if (projectInfo is null) return String.Empty;
+        return String.Format(CultureInfo.InvariantCulture, "Project: {0} (Run {1}, Clone {2}, Gen {3})",
+            projectInfo.ProjectId, projectInfo.ProjectRun, projectInfo.ProjectClone, projectInfo.ProjectGen);
+    }
+
+    /// <summary>
+    /// Returns a short string that represents the Project (R/C/G) information.
+    /// </summary>
+    public static string ToShortProjectString(this IProjectInfo? projectInfo)
+    {
+        if (projectInfo is null) return String.Empty;
+        return String.Format(CultureInfo.InvariantCulture, "P{0} (R{1}, C{2}, G{3})",
+            projectInfo.ProjectId, projectInfo.ProjectRun, projectInfo.ProjectClone, projectInfo.ProjectGen);
+    }
+}
