26d15dbca21905c91802f93c023a40c26b151b4e
harlam357 <harlam357@gmail.com>
2022-12-26 19:01:34 -0600

    fix: LoggerExtensions - don't use String.Format when there are no args

--- src/HFM.Core/Logging/LoggerExtensions.cs
+++ src/HFM.Core/Logging/LoggerExtensions.cs
@@ -6,26 +6,26 @@ namespace HFM.Core.Logging;
 public static class LoggerExtensions
 {
     public static void Debug(this ILogger logger, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Debug, String.Format(CultureInfo.CurrentCulture, message, args));
+        logger.Log(LoggerLevel.Debug, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args));
 
     public static void Debug(this ILogger logger, Exception? exception, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Debug, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+        logger.Log(LoggerLevel.Debug, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args), exception);
 
     public static void Info(this ILogger logger, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Info, String.Format(CultureInfo.CurrentCulture, message, args));
+        logger.Log(LoggerLevel.Info, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args));
 
     public static void Info(this ILogger logger, Exception? exception, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Info, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+        logger.Log(LoggerLevel.Info, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args), exception);
 
     public static void Warn(this ILogger logger, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Warn, String.Format(CultureInfo.CurrentCulture, message, args));
+        logger.Log(LoggerLevel.Warn, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args));
 
     public static void Warn(this ILogger logger, Exception? exception, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Warn, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+        logger.Log(LoggerLevel.Warn, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args), exception);
 
     public static void Error(this ILogger logger, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Error, String.Format(CultureInfo.CurrentCulture, message, args));
+        logger.Log(LoggerLevel.Error, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args));
 
     public static void Error(this ILogger logger, Exception? exception, string message, params object?[] args) =>
-        logger.Log(LoggerLevel.Error, String.Format(CultureInfo.CurrentCulture, message, args), exception);
+        logger.Log(LoggerLevel.Error, args.Length == 0 ? message : String.Format(CultureInfo.CurrentCulture, message, args), exception);
 }
