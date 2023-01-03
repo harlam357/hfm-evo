e63e089ce8f092bbb72b830527541a6ef44332ae
harlam357 <harlam357@gmail.com>
2023-01-02 22:55:27 -0600

    init: DateTimeExtensions

---
+++ src/HFM.Core/Internal/DateTimeExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace HFM.Core.Internal;
+
+internal static class DateTimeExtensions
+{
+    internal static string ToShortStringOrEmpty(this DateTime dateTime) =>
+        ToStringOrEmpty(dateTime, $"{dateTime.ToShortDateString()} {dateTime.ToShortTimeString()}");
+
+    private static string ToStringOrEmpty(this IEquatable<DateTime> date, string formattedValue) =>
+        date.Equals(DateTime.MinValue) ? String.Empty : formattedValue;
+}
