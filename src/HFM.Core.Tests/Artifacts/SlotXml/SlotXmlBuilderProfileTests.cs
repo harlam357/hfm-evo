b20b0c8bf9c895298d34247fa27f1b9d5c909446
harlam357 <harlam357@gmail.com>
2023-01-02 23:16:34 -0600

    init: SlotXmlBuilderProfileTests

---
+++ src/HFM.Core.Tests/Artifacts/SlotXml/SlotXmlBuilderProfileTests.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace HFM.Core.Artifacts.SlotXml;
+
+[TestFixture]
+public class SlotXmlBuilderProfileTests
+{
+    [Test]
+    public void AssertConfigurationIsValid()
+    {
+        var configuration = new MapperConfiguration(config => config.AddProfile<SlotXmlBuilderProfile>());
+        configuration.AssertConfigurationIsValid();
+    }
+}
