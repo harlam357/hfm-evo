feda9d2ed1b31e56dcdfd3f1b89f53c4998f1a28
harlam357 <harlam357@gmail.com>
2023-01-02 22:51:42 -0600

    init: DataContractFileSerializer

---
+++ src/HFM.Core/Serializers/DataContractFileSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+using System.Xml;
+
+namespace HFM.Core.Serializers;
+
+public class DataContractFileSerializer<T> : IFileSerializer<T> where T : class, new()
+{
+    public string FileExtension => "xml";
+
+    public string FileTypeFilter => "Xml Files|*.xml";
+
+    public T? Deserialize(string path)
+    {
+        using var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+        var serializer = new DataContractSerializer(typeof(T));
+        return (T?)serializer.ReadObject(fileStream);
+    }
+
+    public void Serialize(string path, T? value)
+    {
+        using var fileStream = new FileStream(path, FileMode.Create, FileAccess.Write);
+        using var xmlWriter = XmlWriter.Create(fileStream, new() { Indent = true });
+        var serializer = new DataContractSerializer(typeof(T));
+        serializer.WriteObject(xmlWriter, value);
+    }
+}
