26f9287ca755b122f6e580d8bbecc71b9ccdac18
harlam357 <harlam357@gmail.com>
2023-01-02 23:23:41 -0600

    init: SlotXmlBuilderProfile

---
+++ src/HFM.Core/Artifacts/SlotXml/SlotXmlBuilderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace HFM.Core.Artifacts.SlotXml;
+
+public class SlotXmlBuilderProfile : Profile
+{
+    public SlotXmlBuilderProfile()
+    {
+        CreateMap<Log.LogLine, LogLine>()
+            .ForMember(dest => dest.Raw, opt => opt.MapFrom(src => LogLine.RemoveInvalidXmlChars(src.Raw)));
+        CreateMap<Proteins.Protein, Protein>()
+            .ForMember(dest => dest.ServerIP, opt => opt.MapFrom(src => src.ServerIP ?? String.Empty))
+            .ForMember(dest => dest.WorkUnitName, opt => opt.MapFrom(src => src.WorkUnitName ?? String.Empty))
+            .ForMember(dest => dest.Core, opt => opt.MapFrom(src => src.Core ?? String.Empty))
+            .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Description ?? String.Empty))
+            .ForMember(dest => dest.Contact, opt => opt.MapFrom(src => src.Contact ?? String.Empty));
+    }
+}
